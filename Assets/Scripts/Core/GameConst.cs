5834b61e7782e839df5ded129e8464b75c5ae3a6
景光 <>
2024-07-25 11:55:01 +0800

    添加了几个新常量

--- Assets/Scripts/Core/GameConst.cs
+++ Assets/Scripts/Core/GameConst.cs
@@ -6,4 +6,6 @@ public class GameConsts
 {
     public static readonly float GRAVITY = 9.8f;
     public static readonly Vector3 START_POSITION = new Vector3(-7.16f, -0.67f, 0.0f);
+    public static readonly Quaternion ZERO_ROTATION = new Quaternion();
+    public static readonly Vector2 START_VELOCITY = new Vector2();
 }
