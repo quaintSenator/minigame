a069cee18b5c53a3249482dbfa97fdcfc4d7817d
HuangZhibin9 <1724072381@qq.com>
2024-08-02 00:54:49 +0800

    节奏记录逻辑，从夜鸟的音频中保存节拍的时间点

---
+++ Assets/Scripts/Audio/RhythmDataFile.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RhythmDataFile : ScriptableObject
+{
+    public List<RhythmData> rhythmDataList = new List<RhythmData>();
+}
