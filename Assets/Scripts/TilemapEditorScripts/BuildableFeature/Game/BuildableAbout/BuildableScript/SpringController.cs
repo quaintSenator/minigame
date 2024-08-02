ea371f84b5e19b51ef2defa9d1e117ab04808a4e
景光 <>
2024-08-02 16:19:42 +0800

    修复弹簧设置参数的bug

--- Assets/Scripts/TilemapEditorScripts/BuildableFeature/Game/BuildableAbout/BuildableScript/SpringController.cs
+++ Assets/Scripts/TilemapEditorScripts/BuildableFeature/Game/BuildableAbout/BuildableScript/SpringController.cs
@@ -3,10 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpringController : BuildableBase {
+public class SpringController : BuildableBase
+{
 
     [SerializeField]
-    private JumpSettings jumpSettings = GameConsts.SPRING_JUMP;
+    private JumpSettings jumpSettings = new JumpSettings();
+    [SerializeField] private bool useDefault = true;
+
+    public override void Init()
+    {
+        if (useDefault)
+        { 
+            jumpSettings = GameConsts.SPRING_JUMP; 
+        }
+    }
+
+    private void Start()
+    {
+        if (useDefault)
+        { 
+            jumpSettings = GameConsts.SPRING_JUMP; 
+        }       
+    }
 
     protected override void TriggerThisBuildable(PlayerController player)
     {
