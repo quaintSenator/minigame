ee9416e73de749d1bb261226a706fbf248c4fe06
HuangZhibin9 <105227132+HuangZhibin9@users.noreply.github.com>
2024-08-30 10:50:03 +0800

    除去多余的editor引用

--- Assets/Scripts/Managers/WindowManager.cs
+++ Assets/Scripts/Managers/WindowManager.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using Sirenix.Utilities;
-using Sirenix.Utilities.Editor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
--- Assets/Scripts/TestForHzb/Player/PlayerController.cs
+++ Assets/Scripts/TestForHzb/Player/PlayerController.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Sirenix.OdinInspector;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using static UnityEngine.Experimental.Rendering.RayTracingAccelerationStructure;
 
 
 
